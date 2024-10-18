b476e519ae91dd591103d893e09bd065eb4a4a19
Shivani Kanswal <shivanikanswal@gmail.com>
2024-10-18 20:24:34 +0530

    Add basic structure

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function subtract(m, n) {
+  return m * n;
+}
+
+add(12, 6);
+multiply(12, 2);
+subtract(23, 7);
